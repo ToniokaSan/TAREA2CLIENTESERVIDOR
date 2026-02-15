eaa911752e61e8901141b649cad3fc77d0535206
ToniokaSan <potoyaleman991@gmail.com>
2026-02-14 21:11:55 -0600

    INICIO DE REPOSITORIO
    INICIO DE TAREA 2 CLIENTE SERVIDOR

---
+++ TAREA2CS/src/main/java/com/mycompany/tarea2cs/TAREA2CS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tarea2cs;
+
+/**
+ *
+ * @author potoy
+ */
+public class TAREA2CS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
